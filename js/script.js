a55d230ea125bd84e082f1a3b33593b22a80f574
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-06-23 21:30:28 +0200

    Add computerMove randomNumber

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ playerMove = 'kamień';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 playerMove = (typeof playerMove == 'number' ? playerMove : 0) + 1;
 playerMove = 'papier';
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  randomNumber = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
