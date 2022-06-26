e0fcbeeb1ebb629f4cc5cf018c84098e9c194335
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-06-26 21:40:30 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 var argButtonName, buttonPaper, buttonRock, buttonScissors, buttonTest;
 
+buttonTest = document.getElementById('button-test');
+
+buttonRock = document.getElementById('button-rock');
+
+buttonPaper = document.getElementById('button-paper');
+
+buttonScissors = document.getElementById('button-scissors');
+
 /**
  * Describe this function...
  */
@@ -53,11 +61,7 @@ console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
 
 }
-buttonTest = document.getElementById('button-test');
 buttonTest.addEventListener('click', function(){ buttonClicked('Guzik TEST') });
-buttonRock = document.getElementById('button-rock');
 buttonRock.addEventListener('click', function(){ buttonClicked('kamień') });
-buttonPaper = document.getElementById('button-paper');
 buttonPaper.addEventListener('click', function(){ buttonClicked('papier') });
-buttonScissors = document.getElementById('button-scissors');
 buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce') });
